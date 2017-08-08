512814f1892b793ecc4e8940f73ff713086551b5
Nihar Patel <nihpat95@gmail.com>
2017-08-09 00:23:24 +0530

    v2.1

--- js/main.js
+++ js/main.js
@@ -1,1 +1,35 @@
 
+  $(window).on('load', function() {
+
+      // will first fade out the loading animation 
+    	$("#loader").fadeOut("slow", function(){
+
+        // will fade out the whole DIV that covers the website.
+        $("#preloader").delay(300).fadeOut("slow");
+
+      }); 
+
+      // intro section animation
+     	if (!$("html").hasClass('no-cssanimations')) {
+
+     		setTimeout(function(){
+
+    			$('body .animate-intro').each(function(ctr) {
+
+					var el = $(this),
+                  animationEfx = el.data('animate');
+
+               if (animationEfx === null || animationEfx === undefined || animationEfx.trim() === "") {
+                 	animationEfx = "fadeInUp";
+               }
+
+              	setTimeout( function () {
+						el.addClass(animationEfx + ' animated');
+					}, ctr * 100);
+
+				});
+					
+			}, 1000);
+     	}   	   	
+
+  	});
